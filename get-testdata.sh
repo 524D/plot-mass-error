d3e979a6e0bf20bb7d591276db560ef7a558b7dd
rjmarissen <r.j.marissen@lumc.nl>
2023-06-23 13:12:03 +0200

    Add script to obtain test data

---
+++ get-testdata.sh
@@ -0,0 +1,6 @@
+wget -O PXD041271-35765_x02282_AN_sb_IMAC_epoxy_HeLa1mg_1.mzid.gz  https://ftp.pride.ebi.ac.uk/pride/data/archive/2023/05/PXD041271/35765_x02282_AN_sb_IMAC_epoxy_HeLa1mg_1.mzid.gz
+gunzip PXD041271-35765_x02282_AN_sb_IMAC_epoxy_HeLa1mg_1.mzid.gz
+
+wget -O PXD041271-35766_x02283_AN_sb_IMAC_maleimide_HeLa1mg_1.mzid.gz https://ftp.pride.ebi.ac.uk/pride/data/archive/2023/05/PXD041271/35766_x02283_AN_sb_IMAC_maleimide_HeLa1mg_1.mzid.gz
+gunzip  PXD041271-35766_x02283_AN_sb_IMAC_maleimide_HeLa1mg_1.mzid.gz
+
